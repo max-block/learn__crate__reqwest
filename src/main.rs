bccc1c2378529de4a75c745d4c4ab3847f4f7c99
Max Block <maxblock@pm.me>
2020-09-26 14:25:42 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 use std::{collections::HashMap, error::Error};
 
-fn main() -> Result<(), Box<dyn Error>> {
+use serde_json::Value;
+
+fn t1() -> Result<(), Box<dyn Error>> {
     let res =
         reqwest::blocking::get("https://httpbin.org/ip")?.json::<HashMap<String, String>>()?;
 
     println!("{:?}", res);
+    Ok(())
+}
 
+fn t2() -> Result<(), Box<dyn Error>> {
+    let res = reqwest::blocking::get("https://httpbin.org/get?q=1")?.json::<Value>()?;
+
+    println!("{:?}", res.pointer("/args/q").unwrap());
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    t1()?;
+    t2()?;
     Ok(())
 }
